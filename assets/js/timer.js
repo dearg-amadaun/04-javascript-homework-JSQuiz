da902ea926183c5f7b78b8e95d8a677d1b12e9d3
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-07-01 18:26:44 -0400

    href fixes

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -7,7 +7,7 @@ var interval = setInterval(function() {
       	
         /*Need to link timer to scoring, and slow down game over prompt */
          $('#countdown').html("<h3>Game Over!</h3>");  
-        return window.location.assign('/assets/html/end-quiz.html');
+        return window.location.assign("/assets/html/end-quiz.html");
     }else{
     	$('#countdown').text(counter);
     }
